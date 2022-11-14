762c262c50677043857ef99229052f30f96b58ed
Pralay Ray <Pray354@d127.org>
2022-11-14 09:39:10 -0600

    added comments for kasper

--- Multiply Final APP/ViewController.swift
+++ Multiply Final APP/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var firstNumberTextField: UITextField!
+    
+    @IBOutlet weak var secondNumberTextField: UITextField!
+    
+    @IBOutlet weak var totalNumberLabel: UILabel!
+    
+    @IBOutlet weak var operaterLabel: UILabel!
+    
+    @IBOutlet weak var segmentedOutlet: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    // calculate (RAY)
+    
+    @IBAction func operatorSegmentedControl(_ sender: Any) {
+        switch segmentedOutlet.selectedSegmentIndex {
+        case 0:
+            greeting = "Hola"
+        case 1:
+            greeting = "Kem Cho"
+        case 2:
+            greeting = "Namaste"
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        }
+        
+    }
+    
+    // kasper (pictures)
+    // kasper (sliders)
+    // kasper (tip)
+    
 
 }
 
