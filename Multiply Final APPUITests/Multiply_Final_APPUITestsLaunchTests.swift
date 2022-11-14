676933e25b64196d2ca7bfbd929428d8d4fb4aaf
Pralay Ray <Pray354@d127.org>
2022-11-14 08:31:22 -0600

    Initial Commit

---
+++ Multiply Final APPUITests/Multiply_Final_APPUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Multiply_Final_APPUITestsLaunchTests.swift
+//  Multiply Final APPUITests
+//
+//  Created by Pralay T Ray (Student) on 11/14/22.
+//
+
+import XCTest
+
+class Multiply_Final_APPUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
